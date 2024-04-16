b4ee4b2927d6429d25ceba18ca79ed9dc1ad8112
Nique1 <dominikasarata@gmail.com>
2024-04-16 16:30:52 +0200

    config: add docker-compose, configure flyway, Add docker-compose file, use postgres image, create docker container to run postgres db.
    Configure flyway, add flyway scripts, create songs table, insert data to songs table.
    
    Refactoring and making corrections with configuration

--- src/main/resources/db/migration/V1.0.1__insert_songs_data.sql
+++ src/main/resources/db/migration/V1.0.1__insert_songs_data.sql
@@ -1,6 +1,6 @@
-INSERT INTO songs(artistname, trackname, tracktime, genre, decade, emotion) VALUES
-('The Beatles', 'Come Together', '04:19:00', 'Rock', 1960, 'Happy'),
-('Led Zeppelin', 'Stairway to Heaven', '08:02:00', 'Rock', 1970, 'Epic'),
-('Michael Jackson', 'Billie Jean', '04:54:00', 'Pop', 1980, 'Energetic'),
-('Nirvana', 'Smells Like Teen Spirit', '05:01:00', 'Grunge', 1990, 'Angry'),
-('Adele', 'Hello', '04:55:00', 'Pop', 2010, 'Sad');
+INSERT INTO songs (artistname, trackname, tracklength, year) VALUES
+('The Beatles', 'Come Together', 259, 1960),
+('Led Zeppelin', 'Stairway to Heaven', 482, 1970),
+('Michael Jackson', 'Billie Jean', 294, 1980),
+('Nirvana', 'Smells Like Teen Spirit', 301, 1990),
+('Adele', 'Hello', 295, 2010);
