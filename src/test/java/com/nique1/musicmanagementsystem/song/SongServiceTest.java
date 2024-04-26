0393134000ff447d05a11885a9cb887f62d5b258
Nique1 <dominikasarata@gmail.com>
2024-04-26 12:57:41 +0200

    test: test song service and song controller functionalities

--- src/test/java/com/nique1/musicmanagementsystem/song/SongServiceTest.java
+++ src/test/java/com/nique1/musicmanagementsystem/song/SongServiceTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-@SpringBootTest
+
 class SongServiceTest {
     @Mock
     private SongRepository songRepository;
