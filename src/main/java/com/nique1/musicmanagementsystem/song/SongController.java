cd980aa28956878d1e57c60b2f79524fdcfa6eee
Nique1 <dominikasarata@gmail.com>
2024-04-27 21:18:24 +0200

    func: changed controller annotation

--- src/main/java/com/nique1/musicmanagementsystem/song/SongController.java
+++ src/main/java/com/nique1/musicmanagementsystem/song/SongController.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/songs")
 public class SongController {
     private final SongService songService;
