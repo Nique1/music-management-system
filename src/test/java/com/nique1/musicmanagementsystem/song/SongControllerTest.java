22258c6036b59a463e3634697c6f031971fd872a
Nique1 <dominikasarata@gmail.com>
2024-04-27 21:14:38 +0200

    test: test song service and song controller functionalities

--- src/test/java/com/nique1/musicmanagementsystem/song/SongControllerTest.java
+++ src/test/java/com/nique1/musicmanagementsystem/song/SongControllerTest.java
@@ -2,43 +2,52 @@ package com.nique1.musicmanagementsystem.song;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class SongControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @MockBean
-    private SongService songService;
 
+class SongControllerTest {
+
+    private MockMvc mockMvc;
+    @Mock
+    private SongService songService;
+    @InjectMocks
+    private SongController songController;
     private SongEntity song;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(songController).build();
+
         song = new SongEntity(1, "ArtistName", "TrackName", 300, 2020);
     }
 
     @Test
     void findSongBySongId() throws Exception {
-        when(songService.getSongsById(1)).thenReturn(Arrays.asList(song));
+        when(songService.getSongsById(1)).thenReturn(Collections.singletonList(song));
 
         mockMvc.perform(get("/songs/song/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[{'id': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'duration': 300, 'year': 2020}]"));
+                .andExpect(content().json("[{'songId': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'trackLength': 300, 'year': 2020}]"));
     }
 
     @Test
@@ -48,7 +57,7 @@ class SongControllerTest {
         mockMvc.perform(get("/songs/artist/ArtistName")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[{'id': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'duration': 300, 'year': 2020}]"));
+                .andExpect(content().json("[{'songId': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'trackLength': 300, 'year': 2020}]"));
     }
 
     @Test
@@ -58,7 +67,7 @@ class SongControllerTest {
         mockMvc.perform(get("/songs/track/TrackName")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[{'id': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'duration': 300, 'year': 2020}]"));
+                .andExpect(content().json("[{'songId': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'trackLength': 300, 'year': 2020}]"));
     }
 
     @Test
@@ -68,6 +77,6 @@ class SongControllerTest {
         mockMvc.perform(get("/songs/year/2020")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[{'id': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'duration': 300, 'year': 2020}]"));
+                .andExpect(content().json("[{'songId': 1, 'artistName': 'ArtistName', 'trackName': 'TrackName', 'trackLength': 300, 'year': 2020}]"));
     }
 }
---
+++ src/test/java/com/nique1/musicmanagementsystem/song/SongServiceImplTest.java
@@ -0,0 +1,133 @@
+package com.nique1.musicmanagementsystem.song;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+class SongServiceImplTest {
+
+    @Mock
+    private SongRepository songRepository;
+
+    @InjectMocks
+    private SongServiceImpl songService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldReturnTrueWhenSongFoundById() {
+        //given
+        int songId = 1;
+        SongEntity song = new SongEntity(songId, "ArtistName", "TrackName", 300, 2020);
+
+        //when
+        when(songRepository.findSongsBySongId(songId)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsById(songId);
+        //then
+        assertThat(songsResult).isNotEmpty();
+
+    }
+
+    @Test
+    void shouldReturnSongDataWhenSongFoundById() {
+        //given
+        int songId = 1;
+        SongEntity song = new SongEntity(songId, "ArtistName", "TrackName", 300, 2020);
+        //when
+        when(songRepository.findSongsBySongId(songId)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsById(songId);
+        //then
+        assertThat(songsResult).isEqualTo(Arrays.asList(song));
+    }
+
+    @Test
+    void shouldReturnTrueWhenSongFoundByArtistName() {
+        //given
+        String artistName = "ArtistName";
+        SongEntity song = new SongEntity(1, artistName, "TrackName", 300, 2020);
+
+        //when
+        when(songRepository.findSongsByArtistName(artistName)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsByArtistName(artistName);
+        //then
+        assertThat(songsResult).isNotEmpty();
+
+    }
+
+    @Test
+    void shouldReturnSongDataWhenSongFoundByArtistName() {
+        //given
+        String artistName = "ArtistName";
+        SongEntity song = new SongEntity(1, artistName, "TrackName", 300, 2020);
+        //when
+        when(songRepository.findSongsByArtistName(artistName)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsByArtistName(artistName);
+        //then
+        assertThat(songsResult).isEqualTo(Arrays.asList(song));
+    }
+
+    @Test
+    void shouldReturnTrueWhenSongFoundByTrackName() {
+        //given
+        String trackName = "TrackName";
+        SongEntity song = new SongEntity(1, "ArtistName", trackName, 300, 2020);
+
+        //when
+        when(songRepository.findSongsByTrackName(trackName)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsByTrackName(trackName);
+        //then
+        assertThat(songsResult).isNotEmpty();
+
+    }
+
+    @Test
+    void shouldReturnSongDataWhenSongFoundByTrackName() {
+        //given
+        String trackName = "TrackName";
+        SongEntity song = new SongEntity(1, "ArtistName", trackName, 300, 2020);
+        //when
+        when(songRepository.findSongsByTrackName(trackName)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsByTrackName(trackName);
+        //then
+        assertThat(songsResult).isEqualTo(Arrays.asList(song));
+    }
+
+    @Test
+    void shouldReturnTrueWhenSongFoundByYear() {
+        //given
+        int year = 2020;
+        SongEntity song = new SongEntity(1, "ArtistName", "TrackName", 300, year);
+
+        //when
+        when(songRepository.findSongsByYear(year)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsByYear(year);
+        //then
+        assertThat(songsResult).isNotEmpty();
+
+    }
+
+    @Test
+    void shouldReturnSongDataWhenSongFoundByYear() {
+        //given
+        int year = 2020;
+        SongEntity song = new SongEntity(1, "ArtistName", "TrackName", 300, year);
+        //when
+        when(songRepository.findSongsByYear(year)).thenReturn(Arrays.asList(song));
+        List<SongEntity> songsResult = songService.getSongsByYear(year);
+        //then
+        assertThat(songsResult).isEqualTo(Arrays.asList(song));
+    }
+
+}
