572b6e396db93a456f2ac3b2bcc8e40e1a9dc973
Nique1 <dominikasarata@gmail.com>
2024-04-30 18:50:35 +0200

    func: create song controller functionality

--- src/main/java/com/nique1/musicmanagementsystem/song/SongController.java
+++ src/main/java/com/nique1/musicmanagementsystem/song/SongController.java
@@ -1,11 +1,8 @@
 package com.nique1.musicmanagementsystem.song;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,28 +11,22 @@ import java.util.List;
 public class SongController {
     private final SongService songService;
 
-    @Autowired
     public SongController(SongService songService) {
         this.songService = songService;
     }
 
-    @GetMapping("song/{id}")
-    public List<SongEntity> findSongBySongId(@PathVariable int id) {
-        return songService.getSongsById(id);
+    @GetMapping("{id}")
+    public SongEntity findSongBySongId(@PathVariable int id) {
+        return songService.getSongsById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @GetMapping("artist/{artistName}")
-    public List<SongEntity> findSongByArtistName(@PathVariable String artistName) {
-        return songService.getSongsByArtistName(artistName);
+    @GetMapping("search")
+    public List<SongEntity> findSongByArtistTrackOrYear(@RequestParam(required = false) String artistName,
+                                                        @RequestParam(required = false) String trackName,
+                                                        @RequestParam(required = false) Integer year) {
+        return songService.getSongsByArtistTrackOrYear(artistName, trackName, year)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    @GetMapping("track/{trackName}")
-    public List<SongEntity> findSongByTrackName(@PathVariable String trackName) {
-        return songService.getSongsByTrackName(trackName);
-    }
-
-    @GetMapping("year/{year}")
-    public List<SongEntity> findSongByYear(@PathVariable int year) {
-        return songService.getSongsByYear(year);
-    }
 }
