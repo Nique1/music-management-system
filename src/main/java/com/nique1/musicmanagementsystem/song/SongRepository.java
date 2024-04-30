cbca125ac90f73e677383611366d282cd15682e7
Nique1 <dominikasarata@gmail.com>
2024-04-30 18:51:11 +0200

    func: create song repository functionality

--- src/main/java/com/nique1/musicmanagementsystem/song/SongRepository.java
+++ src/main/java/com/nique1/musicmanagementsystem/song/SongRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SongRepository extends JpaRepository<SongEntity, Integer> {
 
-    List<SongEntity> findSongsBySongId(int id);
-    List<SongEntity> findSongsByArtistName(String artistName);
+    Optional<SongEntity> findSongsBySongId(int id);
+    Optional<List<SongEntity>> findSongsByArtistNameOrTrackNameOrYear(String artistName, String trackName, Integer year);
     List<SongEntity> findSongsByTrackName(String trackName);
-    List<SongEntity> findSongsByYear(int year);
+
 }
