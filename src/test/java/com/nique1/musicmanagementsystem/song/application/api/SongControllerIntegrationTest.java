6e85e79ec39f91e682f7c474e88dad1695b2815d
Nique1 <dominikasarata@gmail.com>
2024-06-11 22:26:32 +0200

    fix test: change mockMvc implementation in ControllerIntegrationTest

--- src/test/java/com/nique1/musicmanagementsystem/song/application/api/SongControllerIntegrationTest.java
+++ src/test/java/com/nique1/musicmanagementsystem/song/application/api/SongControllerIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.nique1.musicmanagementsystem.song.application.api;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
